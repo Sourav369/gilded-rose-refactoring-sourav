908b4124ebb1655846cbe7da8081f161d2de9619
Sourav369 <souravmadan264@gmail.com>
2026-04-18 15:15:32 +1000

    refactor: improve readability by introducing constants and consolidating expiry logic

--- src/GildedRose.Console/GildedRose.cs
+++ src/GildedRose.Console/GildedRose.cs
@@ -4,6 +4,11 @@ namespace GildedRose.Console
 {
     public class GildedRoseService
     {
+        private const int MaxQuality = 50;
+        private const string AgedBrie = "Aged Brie";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+
         private readonly IList<Item> _items;
 
         public GildedRoseService(IList<Item> items)
@@ -21,78 +26,59 @@ namespace GildedRose.Console
 
         private void UpdateItem(Item item)
         {
-            if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
-                UpdateSulfuras(item);
+            if (item.Name == Sulfuras)
                 return;
-            }
 
-            if (item.Name == "Aged Brie")
-            {
+            if (item.Name == AgedBrie)
                 UpdateAgedBrie(item);
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                UpdateBackstage(item);
-            }
+            else if (item.Name == BackstagePass)
+                UpdateBackstagePass(item);
             else
-            {
                 UpdateNormalItem(item);
-            }
 
             item.SellIn--;
-
-            if (item.SellIn < 0)
-            {
-                UpdateExpiredItem(item);
-            }
         }
 
-        //  ITEM-SPECIFIC METHODS
+        //  ITEM-SPECIFIC METHODS (CLEANED)
 
         private void UpdateNormalItem(Item item)
         {
             DecreaseQuality(item);
+
+            if (item.SellIn <= 0)
+            {
+                DecreaseQuality(item);
+            }
         }
 
         private void UpdateAgedBrie(Item item)
         {
             IncreaseQuality(item);
-        }
 
-        private void UpdateBackstage(Item item)
-        {
-            IncreaseQuality(item);
-
-            if (item.SellIn < 11)
-            {
-                IncreaseQuality(item);
-            }
-
-            if (item.SellIn < 6)
+            if (item.SellIn <= 0)
             {
                 IncreaseQuality(item);
             }
         }
 
-        private void UpdateSulfuras(Item item)
+        private void UpdateBackstagePass(Item item)
         {
-            // Do nothing
-        }
-
-        private void UpdateExpiredItem(Item item)
-        {
-            if (item.Name == "Aged Brie")
-            {
-                IncreaseQuality(item);
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.SellIn <= 0)
             {
                 item.Quality = 0;
+                return;
             }
-            else if (item.Name != "Sulfuras, Hand of Ragnaros")
+
+            IncreaseQuality(item);
+
+            if (item.SellIn <= 10)
             {
-                DecreaseQuality(item);
+                IncreaseQuality(item);
+            }
+
+            if (item.SellIn <= 5)
+            {
+                IncreaseQuality(item);
             }
         }
 
@@ -100,7 +86,7 @@ namespace GildedRose.Console
 
         private void IncreaseQuality(Item item)
         {
-            if (item.Quality < 50)
+            if (item.Quality < MaxQuality)
             {
                 item.Quality++;
             }
